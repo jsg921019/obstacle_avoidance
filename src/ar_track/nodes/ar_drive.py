616fb5b8eb49114a2535f477b77db77fd7f780bd
JungSG <jsg921019@gmail.com>
2021-02-18 12:11:14 +0900

    [Update] ar_drive.py

--- src/ar_track/nodes/ar_drive.py
+++ src/ar_track/nodes/ar_drive.py
@@ -1,97 +1,107 @@
 #! /usr/bin/env python
 
 import rospy, math
-import cv2, time, rospy
+import cv2
 import numpy as np
 
 from ar_track_alvar_msgs.msg import AlvarMarkers
 from tf.transformations import euler_from_quaternion
 from std_msgs.msg import Int32MultiArray
-from pathfinding.dubins import dubins_path_planning
-from control.stanley import Stanley
 
-xycar_msg = Int32MultiArray()
+class Xycar(object):
+    def __init__(self):
+        # rospy stuffs
+        self.sub = rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.callback, queue_size = 1)
+        self.pub = rospy.Publisher('xycar_motor_msg', Int32MultiArray, queue_size = 1)
+        self.msg = Int32MultiArray()
+        self.window = cv2.namedWindow("AR tag position", cv2.WINDOW_AUTOSIZE|cv2.WINDOW_GUI_NORMAL)
 
-arData = {"DX":0.0, "DY":0.0, "DZ":0.0, "AX":0.0, "AY":0.0, "AZ":0.0, "AW":0.0}
-roll, pitch, yaw = 0, 0, 0
-started = False
-refreshed = False
-steer = Stanley(k=2.0, L=0.0)
+        # xycar pose
+        self.x, self.y, self.z = 0.0, 0.0, 0.0
+        self.yaw = 0.0
+        self.l = 0.0
 
-def callback(msg):
-    global refreshed
-    for i in msg.markers:
-        arData["DX"] = i.pose.pose.position.x
-        arData["DY"] = i.pose.pose.position.y
-        arData["DZ"] = i.pose.pose.position.z
+        #xycar velocity
+        self.speed = 0.0
+        self.reverse = False
 
-        arData["AX"] = i.pose.pose.orientation.x
-        arData["AY"] = i.pose.pose.orientation.y
-        arData["AZ"] = i.pose.pose.orientation.z
-        arData["AW"] = i.pose.pose.orientation.w
-    refreshed = True
+        # control coef
+        self.k = 1.0
+        self.acc = 3.0
 
-rospy.init_node('ar_drive_info')
-rospy.Subscriber('ar_pose_marker', AlvarMarkers, callback)
-motor_pub = rospy.Publisher('xycar_motor_msg', Int32MultiArray, queue_size = 1 )
-xycar_msg = Int32MultiArray()
+    def callback(self, msg):
+        for i in msg.markers:     
+            pose = i.pose.pose
+            self.x = pose.position.x
+            self.y = pose.position.y
+            self.z = pose.position.z
+            self.yaw = euler_from_quaternion((pose.orientation.x, pose.orientation.y,
+                                              pose.orientation.z, pose.orientation.w))[2]
+
+    def control(self):
+
+        # angle
+        self.l = math.hypot(self.x, self.y)
+        cte = - self.l * math.sin(self.yaw)
+        yaw_term = self.yaw + math.atan2(self.x, self.y)
+        cte_term = math.atan2(self.k * cte, 200.0)
+        if self.reverse: yaw_term *= -1.0
+        angle = 5.0 /2.0 * math.degrees(yaw_term + cte_term)
+
+        # speed
+        if self.reverse:
+            target_speed = -30 + max(0, self.y - 200)*0.1
+            if self.y > 300 :
+                self.speed = 0
+                self.reverse = False
+                self.k = 1.0
+            else:
+                if self.speed <= target_speed + self.acc:
+                    self.speed += self.acc
+                elif self.speed + self.acc >= target_speed:
+                    self.speed -= self.acc
+                else:
+                    self.speed = target_speed
+        else:
+            self.k = 1.0
+            target_speed = 50 - max(0, 350 -self.y) * 0.13
+            if 0 < self.y < 70 :
+                self.speed = 0
+                if not(abs(yaw_term) < math.radians(2) and abs(cte_term) < math.radians(2)):
+                    self.reverse = True
+                    self.k = 3.0
+            else:
+                if self.speed <= target_speed + self.acc:
+                    self.speed += self.acc
+                elif self.speed + self.acc >= target_speed:
+                    self.speed -= self.acc
+                else:
+                    self.speed = target_speed
+
+        # publish
+        self.msg.data = [angle, self.speed]
+        self.pub.publish(self.msg)
+
+    def show(self):
+        img = np.zeros((100, 500, 3))
+        cv2.line(img, (20,65), (480, 65), (0, 0, 255), 1)
+        cv2.line(img, (20,50), (20, 80), (0, 0, 255), 2)
+        cv2.line(img, (480,50), (480, 80), (0, 0, 255), 2)
+        cv2.line(img, (250,50), (250, 80), (0, 0, 255), 2)
+        point = (250 + int(230*self.x/self.y), 65) if self.y else (250, 65)
+        cv2.circle(img, point, 13, (0, 255, 0, 100), -1)
+        cv2.putText(img, 'DX:%4d   DY:%4d   Yaw:%3.1f'%(self.x, self.y, math.degrees(self.yaw)), (20, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
+        cv2.putText(img, '%3d pixel'%self.l, (360, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255))
+        cv2.imshow("AR tag position", img)
+        cv2.waitKey(1)
+
+# main
+
+rospy.init_node('ar_drive')
 rate = rospy.Rate(10)
-k = 0.8
-
+xycar = Xycar()
 while not rospy.is_shutdown():
-    speed = 8
-    roll, pitch, yaw = euler_from_quaternion((arData["AX"], arData["AY"], arData["AZ"], arData["AW"]))
-    l = np.hypot(arData["DX"], arData["DY"])
-    cte = -l * np.sin(yaw)
-    yaw_term = yaw + np.arctan2(arData["DX"], arData["DY"])
-    cte_term = np.arctan2(k*cte, 20.0 * speed)
-    angle = yaw_term + cte_term
-    print('--')
-    print("yaw term :", yaw_term)
-    print("cte term :", cte_term)
-    angle = 5. /2. * np.degrees(angle) #arData["DX"]*0.5
-    print('--')
-    # print(refreshed)
-    # if not started:
-    #     if arData["DY"] > 0 :
-    #         started = True
-    #         l = np.hypot(arData["DX"], arData["DY"])
-    #         x = -l * np.sin(yaw)
-    #         y = l * np.cos(yaw)
-    #         path_x, path_y, path_yaw, mode, path_length = dubins_path_planning(x, y, yaw, 0.0, 250.0, -np.pi / 2.0, 0.005, 0.05)
-    #     rate.sleep()
-    #     continue
-
-    # speed = 10.0
-
-    # if refreshed:
-    #     roll, pitch, yaw = euler_from_quaternion((arData["AX"], arData["AY"], arData["AZ"], arData["AW"]))
-    #     l = np.hypot(arData["DX"], arData["DY"])
-    #     x = -l * np.sin(yaw)
-    #     y = l * np.cos(yaw)
-    #     yaw = np.arctan2(-y, -x) + np.arctan2(arData["DX"], arData["DY"])
-    #     refreshed = False
-    # else:
-    #     x += 20 * speed  * np.cos(yaw)
-    #     y += 20* speed * np.sin(yaw)
-    #     yaw += 20* speed * np.tan(steer_angle) / 75.0
-
-    # print("=======================")
-    # print(" x :", round(x,0))
-    # print(" y :", round(y,0))
-    # print(" yaw :", yaw)
-    # print("angle :" , angle)
-    # steer_angle = steer.feedback(x, y, yaw, 20*speed, path_x, path_y, path_yaw, dt=0.1)
-
-    # angle = -np.degrees(steer_angle) * 5.0 / 2.0
-    
-    
-    # if y < 70 :
-    #     print("stop")
-    #     speed = 0
-    #     started = False
-    xycar_msg.data = [angle, speed]
-    motor_pub.publish(xycar_msg)
-
+    xycar.control()
+    xycar.show()
     rate.sleep()
-
+cv2.destroyAllWindows()
--- src/xycar_sim_parking/src/main.py
+++ src/xycar_sim_parking/src/main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
-
+import sys
+import os
+import signal
 import start
-
+def signal_handler(sig, frame):
+    os.system('killall -9 python rosout')
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 start.main()
